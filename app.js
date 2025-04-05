072a9a429e749fa54ab778fceba89b19c1bc9fef
Dagi Ameha <dagimameha@gmail.com>
2025-04-05 23:06:42 +0300

    adding sercurity assurance to the application

--- app.js
+++ app.js
@@ -3,7 +3,43 @@ const userRoute = require('./routes/tourRoute');
 const express = require('express');
 const app = express();
 
-// Global Middlewares
+//  Global middleware
+// set security HTTp headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 app.use(express.json());
 app.use(express.static('public'));
 
