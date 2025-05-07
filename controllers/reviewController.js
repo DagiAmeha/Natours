7889f50f5ef97212346e11082427c40effe5ef41
Dagi Ameha <dagimameha@gmail.com>
2025-05-07 23:16:35 +0300

    routes related to Review resource

---
+++ controllers/reviewController.js
@@ -0,0 +1,79 @@
+const catchAsync = require('../utils/catchAsync');
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/AppError');
+
+exports.setTourUserIds = (req, res, next) => {
+  // allow nexted routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
+  next();
+};
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    statsu: 'success',
+    length: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new AppError('No review found with this id', 404));
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
+    },
+  });
+});
+exports.createReview = catchAsync(async (req, res, next) => {
+  const { review, rating, tour, user } = req.body;
+
+  const newReview = await Review.create({ review, rating, tour, user });
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+exports.updateReview = catchAsync(async (req, res, next) => {
+  const updatedReview = await Review.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  if (!updatedReview) {
+    return next(new AppError('No review found with this id', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review: updatedReview,
+    },
+  });
+});
+
+exports.deleteReview = catchAsync(async (req, res, next) => {
+  await Review.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
